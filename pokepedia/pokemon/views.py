e163e35e3b6e27269372a9f2b4967738b5d74602
Emmanuel Plata <negrete93@gmail.com>
2017-03-02 15:17:11 -0600

    Añadidos archivos de serializadores y vistas

--- pokepedia/pokemon/views.py
+++ pokepedia/pokemon/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from Pokemon.models import Generation, Pokemon, Variation, Type, Ability, VariationType, AbilityDescription
 
 # Create your views here.
--- pokepedia/pokepedia/settings.py
+++ pokepedia/pokepedia/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',	
     'pokemon',
     "items",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
